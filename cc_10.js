5529768b14e88a6adec3c02685d87d6e0e2b93a9
Vanessa Do <xex.vanessa@gmail.com>
2025-02-23 11:50:07 -0500

    Task 2- Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,3 +22,25 @@ console.log(prod1.getDetails());// logged prod1
 prod1.updateStock(3); // updating the stock value of prod1
 console.log(prod1.getDetails()); // logged the new stock value 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+// Task 2: Creating an Order Class 
+// created a class for orders 
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity); // reduces the stock when orders are placed 
+    }
+    getOrderDetails() {
+        const totalPrice = this.product.price * this.quantity; // calculate the total price of the order 
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, 
+        Quantity: ${this.quantity}, Total Price: $${totalPrice}`; // formatted a string of order details 
+    }
+}
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
